730513010f975b0d6b2bf90309bcf90620f358fb
BARHOUNI <younes-barhouni@users.noreply.github.com>
2024-03-17 20:51:55 +0000

    init plugin

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import ora from 'ora';
+import chalk from 'chalk';
+import { BuildOptions, BuildResult, Plugin, PluginBuild } from 'esbuild';
+
+interface IOptions {
+  name?: string;
+  message?: string;
+}
+
+export default (options: IOptions = {}): Plugin => {
+  const name = options.name || 'App';
+  const message = options.message || 'Building started';
+  const spinner = ora();
+
+  return {
+    name: 'progress',
+    setup(build: PluginBuild) {
+      let time: number;
+      build.onStart(() => {
+        spinner.text = `${message}\n`;
+        spinner.start();
+        time = new Date() as unknown as number;
+        console.log(`Build started for ${chalk.green(name)}`);
+      });
+      build.onEnd((result: BuildResult<BuildOptions>) => {
+        result.errors.length ? spinner.fail(`Build failed. ${result.errors.length} error${result.errors.length > 1 ? 's' : ''}`) : spinner.succeed('Build successful');
+        console.log(`Build ended ${chalk.green(name)}: ${chalk.yellow(` ⏱️ ${(new Date() as unknown as number) - time}ms`)}`);
+      });
+    },
+  };
+};
