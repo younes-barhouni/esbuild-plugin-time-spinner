991af2bf50f3c9858ca282c417081edb692437fd
BARHOUNI <younes-barhouni@users.noreply.github.com>
2024-03-18 11:12:08 +0000

    add interface and spinner option

--- README.md
+++ README.md
@@ -8,3 +8,33 @@ Log esbuild progress spinner and build time
   [![GitHub Pull Requests](assets/pull-requests.svg)](https://github.com/younes-barhouni/esbuild-plugin-time-spinner/pulls)
 
 </div>
+
+## Install
+```js
+// if using npm
+npm i -D esbuild-plugin-time-spinner
+// if using yarn
+yarn add -D esbuild-plugin-time-spinner
+// if using pnpm
+pnpm i -D esbuild-plugin-time-spinner
+```
+
+## Usage example
+```ts
+import esbuild from "esbuild";
+import logTimeSpinner from "esbuild-plugin-time-spinner"
+esbuild.build({
+  entryPoints: ['./index.tsx'],
+  bundle: false,
+  outdir: 'out',
+  plugins: [
+    logTimeSpinner()
+  ]
+})
+```
+
+![alt text](image-1.png)
+![alt text](image.png)
+
+## Options
+When instantiating plugin you can pass an objects with options. This object has SWC official `Compilation` type, see detail in [Compilation](https://swc.rs/docs/configuration/compilation)。
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import ora from 'ora';
+import ora, { Spinner } from 'ora';
+import {type SpinnerName} from 'cli-spinners';
 import chalk from 'chalk';
 import { BuildOptions, BuildResult, Plugin, PluginBuild } from 'esbuild';
 
-interface IOptions {
+export interface IOptions {
   name?: string;
   message?: string;
+  spinner?: SpinnerName | Spinner;
 }
 
 export default (options: IOptions = {}): Plugin => {
-  const name = options.name || 'App';
-  const message = options.message || 'Building started';
-  const spinner = ora();
+  const name: string = options.name || 'App';
+  const message: string = options.message || 'Building started';
+  const spinner: SpinnerName | Spinner = options.spinner || 'dots';
+  const oraSpinner = ora();
 
   return {
     name: 'progress',
@@ -18,12 +21,13 @@ export default (options: IOptions = {}): Plugin => {
       let time: number;
       build.onStart(() => {
         console.log(`Build started for ${chalk.green(name)}`);
-        spinner.text = `${message}\n`;
-        spinner.start();
+        oraSpinner.text = `${message}\n`;
+        oraSpinner.spinner = spinner;
+        oraSpinner.start();
         time = new Date() as unknown as number;
       });
       build.onEnd((result: BuildResult<BuildOptions>) => {
-        result.errors.length ? spinner.fail(`Build failed. ${result.errors.length} error${result.errors.length > 1 ? 's' : ''}`) : spinner.succeed('Build successful');
+        result.errors.length ? oraSpinner.fail(`Build failed. ${result.errors.length} error${result.errors.length > 1 ? 's' : ''}`) : oraSpinner.succeed('Build successful');
         console.log(`Build ended ${chalk.green(name)}: ${chalk.yellow(` ⏱️ ${(new Date() as unknown as number) - time}ms`)}`);
       });
     },
